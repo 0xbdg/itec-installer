a836bca569ec3d3cb146f24ffa473d979d30bb9c
0xbdg <benjamindguntara@protonmail.com>
2025-07-28 23:54:40 +0700

    pdsapdksa

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from dialog import Dialog
+
+d = Dialog(dialog="dialog",autowidgetsize=True)
+
+d.set_background_title("ITEC-OS")
--- src/main.py
+++ src/main.py
@@ -4,19 +4,25 @@ d = Dialog(dialog="dialog",autowidgetsize=True)
 
 d.set_background_title("ITEC-OS")
 
-if d.yesno("Welcome to ITEC-OS installer\nDo you want to continue?") == d.OK: 
+def menu():
+    if d.msgbox("Welcome to ITEC-OS installer. Minimum installer for archiso Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", width=50, height=10) == d.OK: 
+ 
+        code, tags = d.menu("What sandwich toppings do you like?",
+            choices=[("Timezone", "Set location"),
+                     ("Locale", "Set locale"),
+                     ("Keyboard", "Keyboard layout"),
+                     ("Mirror", "Set mirror"),
+                     ("Partition", "Disk configuration"),
+                     ("User", "User account"),
+                     ("Install", "")
+            ],
+            title="ITEC-OS Installer",
+            width=50, 
+        )
+        if code == d.OK:
+            if tags == "Timezone":
+                d.msgbox("test")
+                print(tags)
 
-    code, tags = d.menu("What sandwich toppings do you like?",
-                             choices=[("Timezone", "Set location"),
-                                      ("Locale", "Set locale"),
-                                      ("Keyboard", "Keyboard layout"),
-                                      ("Mirror", "Set mirror"),
-                                      ("Partition", "Disk configuration"),
-                                      ("User", "User account"),
-                                      ("Install", "")
-                            ],
-                             title="ITEC-OS Installer",)
-    if code == d.OK:
-        if tags == "Timezone":
-            d.msgbox("test")
-        print(tags)
+if __name__ == "__main__":
+    menu()
