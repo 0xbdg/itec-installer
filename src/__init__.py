a529cb6486df608457ce8501c0ffdcdc2568b524
0xbdg <benjamindguntara@protonmail.com>
2025-07-29 06:08:54 +0700

    dasdjais

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from dialog import Dialog
-
-d = Dialog(dialog="dialog",autowidgetsize=True)
-
-d.set_background_title("ITEC-OS")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
+from subprocess import *
 from dialog import Dialog
+from zoneinfo import available_timezones
 
 d = Dialog(dialog="dialog",autowidgetsize=True)
 
 d.set_background_title("ITEC-OS")
 
-def menu():
-    if d.msgbox("Welcome to ITEC-OS installer. Minimum installer for archiso Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", width=50, height=10) == d.OK: 
- 
-        code, tags = d.menu("What sandwich toppings do you like?",
+def timezone():
+    timezones = sorted(available_timezones())
+    formatted_timezones = [(tz, "", False) for tz in timezones]
+
+    code, tag =d.radiolist(text="Select timezone", choices=formatted_timezones)
+
+    if code == d.OK:
+        d.msgbox(f"{tag} set success")
+        menu()
+
+    print(tag)
+
+def welcome():
+     if d.msgbox("Welcome to ITEC-OS installer. Minimum installer for archiso Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", width=50, height=10) == d.OK: 
+        menu()
+
+def menu(): 
+    code, tags = d.menu("",
             choices=[("Timezone", "Set location"),
                      ("Locale", "Set locale"),
                      ("Keyboard", "Keyboard layout"),
@@ -18,11 +34,10 @@ def menu():
             ],
             title="ITEC-OS Installer",
             width=50, 
-        )
-        if code == d.OK:
-            if tags == "Timezone":
-                d.msgbox("test")
-                print(tags)
+    )
+    if code == d.OK:
+        if tags == "Timezone":
+            timezone()
 
 if __name__ == "__main__":
-    menu()
+    welcome()
